482c93bad0c6c704b97f6f1fe25d63e04e721a5d
Juan Carlos Valerio Barreto <jc.valerio.b@gmail.com>
2016-12-05 14:33:40 -0600

    animations

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,48 @@
-import { Component } from '@angular/core';
+import { 
+	Component,
+	trigger,
+	state,
+	style,
+	transition,
+	animate,
+	keyframes
+} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	moduleId: module.id,
+  	selector: 'my-app',
+  	templateUrl: 'app.component.html',
+  	styleUrls: ['app.component.css'],
+  	animations: [
+  		trigger('focusPanel', [
+  				state('inactive', style({
+  					transform: 'scale(1)',
+  					backgroundColor: '#ff0000'
+  				})),
+  				state('active', style({
+  					transform: 'scale(1.5)',
+  					backgroundColor: '#aa3322'
+  				})),
+  				transition('inactive => active', animate('100ms ease-in')),
+  				transition('active => inactive', animate('100ms ease-out'))
+  			]),
+  		trigger('movePanel', [
+  			transition('void => *', [
+  						animate(600,keyframes([
+  								style({opacity: 0, transform: 'translateY(-200px)', offset: 0}),
+  								style({opacity: 1, transform: 'translateY(25px)', offset: .75}),
+  								style({opacity: 1, transform: 'translateY(0)', offset: 1})
+  	
+  							]))
+  					])
+  			])
+  	]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+
+	state: string = 'inactive';
+
+	toggleMove() {
+		this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+	}
+ }
